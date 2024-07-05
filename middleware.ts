cacd8a163830334e7fbe18d8eea392901f857989
phthalos <phthalos64@gmail.com>
2024-07-05 22:17:50 +0900

    ✨ UploadThing setup

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
+    publicRoutes: ["/api/uploadthing"];
     if (isProtectedRoute(req)) auth().protect();
 });
 
